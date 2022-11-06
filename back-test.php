c91658f9fe56415c9efdd5a814f082daf4e913ec
MayurJivani <89565706+MayurJivani@users.noreply.github.com>
2022-11-06 16:28:56 +0530

    Update back-test.php

--- back-test.php
+++ back-test.php
@@ -1,5 +1,10 @@
 <?php 
     session_start();
     echo $_SESSION['TrackToPlay'];
+    echo "<br>";
     echo $_SESSION['TrackName'];
+    echo "<br>";
+    print_r($_SESSION['TrackArray']) ;
+    echo "<br>";
+    print_r($_SESSION['TrackNameArray']) ;
 ?>
