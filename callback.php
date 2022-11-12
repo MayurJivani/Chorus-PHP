07ef8ec4e352d610a306f39a1ee8fe57a1b5b3cb
MayurJivani <mayurjivani24@gmail.com>
2022-11-12 23:19:07 +0530

    update

--- callback.php
+++ callback.php
@@ -2,8 +2,8 @@
     require 'vendor/autoload.php';
     session_start();
     $session = new SpotifyWebAPI\Session(
-        '72c28085aad645aabbe0176ebc4a7bf0',
-        '9c03812f866f416fa6e799f8952b07a2',
+        '13f3f5f7945245e9b8a02a728205f780',
+        'dc405c35d15d47b3a947f4dafeb3d615',
         'http://localhost/Chorus/callback.php'
     );
     
